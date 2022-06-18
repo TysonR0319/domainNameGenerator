02ccf5b3480703e2477832f74afd97871dcd7da9
TysonR0319 <tysonr0319@gmail.com>
2022-06-18 18:18:56 +0000

    final commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#button1").addEventListener("click", () => {
+    document.querySelector("#domaingenerator").innerHTML = generateDomain();
+  });
+
+  let generateDomain = () => {
+    let pronoun = ["our", "the", "a"];
+    let adj = ["best", "great", "amazing"];
+    let subject = ["jogger", "mailman", "artist"];
+    let extensions = [".com", ".io", ".ext"];
+    let domain = [];
+
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < subject.length; k++) {
+          for (let l = 0; l < extensions.length; l++) {
+            domain.push(
+              "www." + pronoun[i] + adj[j] + subject[k] + extensions[l]
+            );
+          }
+        }
+      }
+    }
+    return domain;
+  };
 };
